edc50efa2fb2c247712c06a4eb86625f7a3684d5
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-08-25 21:17:25 -0400

    pass all tests

--- src/components/Gallery/__tests__/index.test.js
+++ src/components/Gallery/__tests__/index.test.js
@@ -1,8 +1,8 @@
+// __tests__/Gallery.test.js
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Gallery from "..";
-
 const portrait = {
   name: "portraits",
   description: "Portraits of people in my life",
@@ -15,13 +15,13 @@ describe("Gallery is rendering", () => {
     render(<Gallery currentCategory={portrait} />);
   });
 
-  it("matches snapshot", () => {
+  it("renders", () => {
     const { asFragment } = render(<Gallery currentCategory={portrait} />);
     expect(asFragment()).toMatchSnapshot();
   });
+});
 
-  it("renders", () => {
-    const { getByTestId } = render(<Gallery currentCategory={portrait} />);
-    expect(getByTestId("h1tag")).toHaveTextContent("Portraits");
-  });
+it("renders", () => {
+  const { getByTestId } = render(<Gallery currentCategory={portrait} />);
+  expect(getByTestId("h1tag")).toHaveTextContent("Portraits");
 });
