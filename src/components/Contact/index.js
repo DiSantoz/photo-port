b4e609c8932b47ad02d024b044b78034a6739a18
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-08-25 21:14:55 -0400

    pass all tests

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
+
 import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
-  const [errorMessage, setErrorMessage] = useState("");
   const [formState, setFormState] = useState({
     name: "",
     email: "",
     message: "",
   });
 
+  const [errorMessage, setErrorMessage] = useState("");
   const { name, email, message } = formState;
 
-  function handleChange(e) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      setFormState({ [e.target.name]: e.target.value });
+      console.log("Form", formState);
+    }
+  };
+
+  const handleChange = (e) => {
     if (e.target.name === "email") {
       const isValid = validateEmail(e.target.value);
-      console.log(isValid);
-      // isValid conditional statement
       if (!isValid) {
         setErrorMessage("Your email is invalid.");
       } else {
@@ -28,28 +35,19 @@ function ContactForm() {
         setErrorMessage("");
       }
     }
-
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-    }
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(formState);
-  }
+  };
 
   return (
     <section>
-      <h1>Contact me</h1>
+      <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
           <input
             type="text"
+            name="name"
             defaultValue={name}
             onBlur={handleChange}
-            name="name"
           />
         </div>
         <div>
@@ -65,17 +63,19 @@ function ContactForm() {
           <label htmlFor="message">Message:</label>
           <textarea
             name="message"
+            rows="5"
             defaultValue={message}
             onBlur={handleChange}
-            rows="5"
           />
-          {errorMessage && (
-            <div>
-              <p className="error-text">{errorMessage}</p>
-            </div>
-          )}
         </div>
-        <button type="submit">Submit</button>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button data-testid="button" type="submit">
+          Submit
+        </button>
       </form>
     </section>
   );
